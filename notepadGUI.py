df4851dc3fd80ecf3bd6850ade6d07ea7348c214
Jerry <jerrynutt416@gmail.com>
2021-05-10 18:07:33 -0400

    Gui changes

--- notepadGUI.py
+++ notepadGUI.py
@@ -6,7 +6,7 @@ from sys import platform
 from datetime import datetime
 import time, os
 
-#What have I learned so far- write.
+
 class Notebook:
   
   def __init__(self):
@@ -22,6 +22,7 @@ class Notebook:
     self.bottom_widget_id = True
     self.scroll_x_id = True
     self.scroll_x = True
+    self.save_option_text = True
     
   def main_hub(self,new_file_type='Nones'):
     self.new_file_type = new_file_type
@@ -29,6 +30,8 @@ class Notebook:
       if new_file_type == 'saved':
         return self.open_saved_file()
       return 0
+    if self.current_file != 'Untitled':
+      self.save_option_text.configure(text="Do you want to save changes to {}?".format(os.path.basename(self.current_file)))
     return self.save_option_frame.pack()
 
   def clear_text(self,option='quit'):
@@ -74,7 +77,10 @@ class Notebook:
       self.new_window.title(os.path.basename(self.current_file))
     except AttributeError:
       return 'Attribute Error'
-  
+  def exit(self):
+    self.new_window.destroy()
+    print(sam)
+    return check_for()  
   #/////////////////////////////////////////////////////////////////////////
   def cut(self):
     self.saved_text = self.text_field.get("sel.first", "sel.last")
@@ -208,40 +214,50 @@ class Notebook:
     return self.example_text_label.configure(font=(self.font[0],self.font[1],self.font[2]))
   
   def set_font(self):
-    self.set_font_frame = tk.Frame(self.text_field, height=500, width=550,highlightthickness=12,background="white")
+    self.set_font_frame = tk.Frame(self.text_field, height=500, width=550,highlightthickness=1)
     self.set_font_frame.pack(anchor='center')
     self.set_font_frame.pack_propagate(0)
+
+    self.font_top = tk.Frame(self.set_font_frame, height=20,width=250,background='white')
+    self.font_top.pack(anchor='center',fill='x')
+    self.font_top_text = tk.Label(self.font_top,font=(8), text="Font",background='white')
+    self.font_top_text.pack(side='left')
+    self.font_top_x = tk.Button(self.font_top, text ="X",command=self.set_font_frame.pack_forget)
+    self.font_top_x.pack(side='right',fill='both')
    
     self.options_widget = tk.Frame(self.set_font_frame, height=300)
+    
     self.options_widget.pack(anchor='n',fill='x')
+   
 
     self.font_widget = tk.Frame(self.options_widget, height=300,width=300)
     self.font_widget.pack(side='left',padx=10)
 
-    self.font_text_label = tk.Label(self.font_widget, text="Font:",background='white')
+    self.font_text_label = tk.Label(self.font_widget,font=(8), text="Font:")
+    
     self.font_text_label.pack(anchor='w')
 
-    self.style_widget = tk.Frame(self.options_widget, height=300,width=120,background="white")
+    self.style_widget = tk.Frame(self.options_widget, height=300,width=120)
     self.style_widget.pack(side='left',padx=10)
-    self.style_text_label = tk.Label(self.style_widget, text="Style:",background='white')
+    self.style_text_label = tk.Label(self.style_widget, font=(8),text="Style:")
     self.style_text_label.pack(anchor='w')
 
-    self.size_widget = tk.Frame(self.options_widget, height=300,width=220,background="white")
+    self.size_widget = tk.Frame(self.options_widget, height=300,width=220)
     self.size_widget.pack(side='left',padx=10)
-    self.size_text_label = tk.Label(self.size_widget, text="Size:",background='white')
+    self.size_text_label = tk.Label(self.size_widget,font=(8), text="Size:")
     self.size_text_label.pack(anchor='w')
 
-    self.example_text_label = tk.Label(self.set_font_frame, height=5, width=20,highlightcolor="black",text="AaBbCcDdEe")
-    self.example_text_label.configure(font=("Lucida Console", 8))
+    self.example_text_label = tk.Label(self.set_font_frame, height=5, width=15,background="#B7B7B7",text="AaBbCcDdEe")
+    self.example_text_label.configure(font=("Lucida Console", 12))
     self.example_text_label.pack(side='right')
 
     self.font_save_widget = tk.Frame(self.set_font_frame, height=5,width=100)
     self.font_save_widget.pack(side='left')
 
-    self.cancel_b = tk.Button(self.font_save_widget,height=3,width=10, text ="Preview",command=lambda: self.change_font('preview'),padx=10)
-    self.cancel_b.pack(side='right')
-    self.save_b = tk.Button(self.font_save_widget,height=3,width=10, text ="Save",command=self.change_font,padx=10)
-    self.save_b.pack(side='right')
+    self.cancel_b = tk.Button(self.font_save_widget,height=2,width=6,font=(8), text ="Preview",command=lambda: self.change_font('preview'),padx=10)
+    self.cancel_b.pack(side='top')
+    self.save_b = tk.Button(self.font_save_widget,height=2,width=6,font=(8), text ="Save",command=self.change_font,padx=10)
+    self.save_b.pack(side='top')
     
     font_family = ['Modern', 'Roman', 'Script', 'Courier', 'MS Serif', 'MS Sans Serif', 
     'Small Fonts', 'Marlett', 'Arial',  'Calibri',  'Candara',  'Consolas', 'Constantia', 'Corbel', 'Courier New', 
@@ -250,7 +266,7 @@ class Notebook:
     'Lucida Sans', 'Magneto','Mistral', 'Onyx',  'Papyrus', 'Pristina', 'Ravie', 
     'Franklin Gothic Demi','Lucida Console']
     font_style = ['bold','normal','italic']
-    font_size = [8,9,10,11,12,14,16,18,20,22,24,26,28,36,48,72]
+    font_size = [8,9,10,11,12,14,16,18,20,22,24,26,28,36]
     
     widget_array = [[self.font_widget,font_family],[self.style_widget,font_style],[self.size_widget,font_size]]
     self.listArray = [0,1,2]
@@ -258,7 +274,8 @@ class Notebook:
       self.scrollbar = tk.Scrollbar(widget_array[i][0])
       self.scrollbar.pack(side='right',fill='y')
       
-      self.listArray[i] = tk.Listbox(widget_array[i][0],height=10,width=20, yscrollcommand = self.scrollbar.set )
+      self.listArray[i] = tk.Listbox(widget_array[i][0],height=10,width=15, yscrollcommand = self.scrollbar.set )
+      self.listArray[i].configure(font=(8))
       self.listArray[i].bind("<Button-1>",self.change_display)
       for line in widget_array[i][1]:
         self.listArray[i].insert('end',str(line))
@@ -276,8 +293,8 @@ class Notebook:
       self.font[1] = 14
       self.zoom_level = 100
     self.text_field.configure(font=(self.font[0],self.font[1],self.font[2]))
-    self.zoom_number_widget.config(text=self.zoom_level)
-    return self.zoom_widget.pack_forget()
+    return self.zoom_number_widget.config(text="{}%".format(self.zoom_level))
+    
   def change_status_bar(self):
     if self.bottom_widget_id.winfo_ismapped() == 1:
       return self.bottom_widget_id.pack_forget()
@@ -292,6 +309,7 @@ class Notebook:
     self.top_menu = tk.Menu(self.new_window)
     self.new_window.config(menu=self.top_menu)
     self.new_window.title('Untitled - Notepad')
+    self.new_window.iconbitmap(r"images/favicon.ico")
 
     def doSomething():
       self.new_window.destroy()
@@ -348,7 +366,7 @@ class Notebook:
     self.file_menu.add_command(label='Open...      Crtl+O',command=lambda: self.main_hub('saved'))
     self.file_menu.add_command(label='Save    Ctrl+S',command=self.save_file)
     self.file_menu.add_command(label='Save as    Crtl+Shift+S',command=self.save_as_file)
-    self.file_menu.add_command(label='Exit',command=self.new_window.destroy)
+    self.file_menu.add_command(label='Exit',command=self.exit)
     
     self.edit_menu.add_command(label='Undo             Ctrl+Z',command=self.text_field.edit_undo)
     self.edit_menu.add_command(label='Cut              Ctrl+X',command=self.cut)
@@ -365,36 +383,28 @@ class Notebook:
     self.format_menu.add_command(label='font..',command=self.set_font)
     
     
-    self.view_menu.add_command(label='Zoom',command=lambda:  self.zoom_widget.pack(anchor='center'))
+    self.view_menu.add_command(label='Zoom In',command=lambda: self.zoom('in'))
+    self.view_menu.add_command(label='Zoom Out',command=lambda: self.zoom('out'))
+    self.view_menu.add_command(label='Restore Default Zoom',command=self.zoom)
     self.view_menu.add_command(label='Status Bar',command=self.change_status_bar)
-
-    self.zoom_widget = tk.Frame(self.text_field, height=200, width=280,highlightthickness=1,background='grey')
-    self.zoom_in = tk.Button(self.zoom_widget, text = "Zoom In",command=lambda: self.zoom('in'))
-    self.zoom_in.pack(anchor='n')
-    self.zoom_out = tk.Button(self.zoom_widget, text = "Zoom Out",command=lambda: self.zoom('out'))
-    self.zoom_out.pack(anchor='n')
-    self.restore = tk.Button(self.zoom_widget, text = "Restore Default Zoom",command=self.zoom)
-    self.restore.pack(anchor='n')
-    self.zoom_widget.pack_propagate(0)
-    self.zoom_widget.pack_forget()
 
     root.bind('<Control-x>',self.cut)
     self.text_field.bind("<Button-1>",self.display_index_location)
     
     self.text_field.pack(expand=True, fill='both')
 
-    self.save_option_frame = tk.Frame(self.text_field, height=200, width=400,highlightthickness=1,background="white")
+    self.save_option_frame = tk.Frame(self.text_field, height=200, width=430,highlightthickness=1,background="white")
     self.save_option_frame.pack(anchor='center')
     self.save_option_frame.pack_propagate(0)
 
-    self.save_option_top = tk.Frame(self.save_option_frame, height=20,width=250,background="white")
+    self.save_option_top = tk.Frame(self.save_option_frame, height=20,width=450,background="white")
     self.save_option_top.pack(anchor='center',fill='both')
-    self.find_text = tk.Label(self.save_option_top, text="Find",font=(16),background="white",padx=10)
-    self.find_text.pack(side='left')
-    self.save_option_x = tk.Button(self.save_option_top, text ="X",background="red",command=self.save_option_frame.pack_forget)
+    self.notepad_text = tk.Label(self.save_option_top, text="Notepad",font=(16),background="white",padx=10)
+    self.notepad_text.pack(side='left')
+    self.save_option_x = tk.Button(self.save_option_top, text ="X",command=self.save_option_frame.pack_forget)
     self.save_option_x.pack(side='right',fill='both')
     self.save_option_text = tk.Label(self.save_option_frame, text="Do you want to save changes to {}?".format(self.current_file),fg='#002266',font = (16),background="white",pady=30)
-    self.save_option_text.pack(anchor='w')
+    self.save_option_text.pack(anchor='center')
     
     self.save_option_button = tk.Frame(self.save_option_frame, height=200,width=250,background="#ebebe0",pady=10,padx=5)
     self.save_option_button.pack(anchor='s',fill='both')
@@ -412,13 +422,12 @@ class Notebook:
     self.white_widget.pack(anchor='center',fill='x')
     self.find_frame_text = tk.Label(self.white_widget, text="Find",background='white')
     self.find_frame_text.pack(side='left')
-    self.find_frame_x = tk.Button(self.white_widget, text ="X",background='red',command=self.find_frame.pack_forget)
+    self.find_frame_x = tk.Button(self.white_widget, text ="X",command=self.find_frame.pack_forget)
     self.find_frame_x.pack(side='right',fill='both')
 
     self.find_frame_content = tk.Frame(self.find_frame, height=120, width=220)
     self.find_frame_content.pack(side="left",fill='x')
     self.find_frame_content.pack_propagate(0)
-    #highlightbackground="black"
     self.find_widget = tk.Frame(self.find_frame_content, highlightbackground="black")
     self.find_widget.pack(anchor='center',fill='x')
     self.find_what = tk.Label(self.find_widget, text="Find What:",background='white')
@@ -447,7 +456,7 @@ class Notebook:
 
     self.find_next_button = tk.Button(self.find_frame_right, text ="Find Next",command=self.find_next)
     self.find_next_button.pack(side='top')
-    self.replace_button = tk.Button(self.find_frame_right, text ="Replace..",command=self.replace_found_str)
+    self.replace_button = tk.Button(self.find_frame_right, text ="Replace",command=self.replace_found_str)
     self.replace_button.pack(side='top')
     self.replace_all_button = tk.Button(self.find_frame_right, text ="Replace All",command=lambda: self.replace_found_str(True))
     self.replace_all_button.pack(side="top")
@@ -469,7 +478,7 @@ class Notebook:
 
     self.new_window.geometry('1000x500')
 
-
+sam = 'k'
 def check_for():
   for i in window_array:
     if i.new_window.winfo_exists() == 1:
@@ -477,12 +486,9 @@ def check_for():
     return root.destroy() 
 if __name__ == "__main__":
   root = tk.Tk()
-  
-
-  
-  
+  root.iconbitmap(r"images/favicon.ico")
   window_array = [] 
-  
+  root.withdraw()
   my_notebook = Notebook()
   
   button_label = tk.Button(root, text='Open',font=(14),pady=10,padx=5)
